86827b94431bbed5d76c23ec26ebce862ace9b99
Chris Brabham <snowind122@gmail.com>
2022-07-27 21:16:47 +0100

    Remove strict mode from Next config

--- apps/reflio/next.config.js
+++ apps/reflio/next.config.js
@@ -3,7 +3,6 @@ require("dotenv").config({ path: "../../.env" });
 const withTM = require("next-transpile-modules")(["ui"]);
 
 module.exports = withTM({
-  reactStrictMode: true,
   images: {
     domains: ['s2.googleusercontent.com'],
   }
