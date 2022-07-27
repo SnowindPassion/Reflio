e0347b75fa902bbe02f869775f664bc4c96bce21
Chris Brabham <snowind122@gmail.com>
2022-07-27 21:26:58 +0100

    Changed from dev dependencies to normal

--- apps/reflio-affiliate/next.config.js
+++ apps/reflio-affiliate/next.config.js
@@ -3,6 +3,7 @@ require("dotenv").config({ path: "../../.env" });
 const withTM = require("next-transpile-modules")(["ui"]);
 
 module.exports = withTM({
+  reactStrictMode: true,
   images: {
     domains: ['s2.googleusercontent.com'],
   }
