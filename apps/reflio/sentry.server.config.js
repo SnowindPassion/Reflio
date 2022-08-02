d07282df9f535a3eec30dbd0a40af9ea30e7772b
Chris Brabham <snowind122@gmail.com>
2022-08-02 22:57:37 +0100

    update: DSN key fix

--- apps/reflio/sentry.server.config.js
+++ apps/reflio/sentry.server.config.js
@@ -7,7 +7,7 @@ import * as Sentry from '@sentry/nextjs';
 const SENTRY_DSN = process.env.NEXT_PUBLIC_SENTRY_DSN;
 
 Sentry.init({
-  dsn: SENTRY_DSN || 'https://fbcd01b0ee5746d7a5aa430f9383a008@o1343834.ingest.sentry.io/6619192',
+  dsn: SENTRY_DSN,
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1.0,
   // ...
